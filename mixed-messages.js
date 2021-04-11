5905bac72e157462fac9634e266360a9494e6763
Hugh <hughmm.fraser@gmail.com>
2021-04-11 20:54:11 +0100

    Initial Commit.

---
+++ mixed-messages.js
@@ -0,0 +1,6 @@
+/**
+ * Message Generator program created for the Codecademy Full Stack Path
+ * Every time the user runs the program, they are presented with a new, randomised output 
+ */
+
+ console.log("This is testing the app works with Node.");
